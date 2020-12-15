413d08c163d296a51d0c3c87294ed2b68f0b5a0b
Malo Mn <malo.maisonneuve@gmail.com>
2020-12-15 20:26:21 +0100

    day-12 & 13 added

--- day-12/rain_risk.py
+++ day-12/rain_risk.py
@@ -1,39 +1,43 @@
 from utils import get_lines
 from typing import Tuple
 import math
-
-
-def add(a: Tuple[int, int], b: Tuple[int, int]):
-    return a[0] + b[0], a[1] + b[1]
-
-
-def int_product(a: Tuple[int, int], b: int)
-    return a[0] * b, a[1] * b
+import numpy as np
 
 
 def move(pos, direct, steps):
-    return add(pos, int_product(direct, steps))
+    return pos + direct.dot(steps)
+
+
+def rotation(point, angle):
+    angle = angle*math.pi/180
+    matrix = np.array([[math.cos(angle), - math.sin(angle)],
+                       [math.sin(angle), math.cos(angle)]])
+    return np.array(point).dot(matrix)
+
 
 if __name__ == "__main__":
     data = get_lines('rain_risk.txt')
     data = [(a[0], int(a[1:])) for a in data]
     print(data)
 
-    orientation = (1, 0)
-    position = (0, 0)
+    waypoint = np.array((10, 1))
+    position = np.array((0, 0))
 
     for d, n in data:
         if d == "N":
-            position = move(position, (0, 1), n)
+            waypoint = move(waypoint, np.array((0, 1)), n)
         elif d == "S":
-            position = move(position, (0, -1), n)
+            waypoint = move(waypoint, np.array((0, -1)), n)
         elif d == "E":
-            position = move(position, (1, 0), n)
+            waypoint = move(waypoint, np.array((1, 0)), n)
         elif d == "W":
-            position = move(position, (-1, 0), n)
+            waypoint = move(waypoint, np.array((-1, 0)), n)
         elif d == "L":
-            pass
+            waypoint = rotation(waypoint, -n)
         elif d == "R":
-            pass
+            waypoint = rotation(waypoint, n)
         elif d == "F":
-            pass
+            position = move(position, waypoint, n)
+        print(d, n, position, waypoint)
+
+    print(round((sum(abs(position)))))

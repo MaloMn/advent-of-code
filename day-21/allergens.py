c82eec854be1ccbd5d284cd67fd3e9665346f2e5
Malo Mn <malo.maisonneuve@gmail.com>
2020-12-22 22:15:37 +0100

    found idea for day-21

---
+++ day-21/allergens.py
@@ -0,0 +1,17 @@
+# a b c d (contains dairy, fish)
+# e f g a (contains dairy)
+# c f (contains soy)
+# c a g (contains fish)
+#
+# g in fish but not in dairy/fish
+# f, g, e in dairy but not in dairy/fish
+# b and d not in dairy nor fish
+# remove f from the list as it is alone in soy
+#
+# b, d, e, f, g
+
+impossibles = set()
+
+
+
+
